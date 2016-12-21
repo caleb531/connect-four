48da3657c446807bb1cfaf3d97c5db1dca0d8a29
Caleb Evans <caleb@calebevans.me>
2016-12-21 13:10:20 -0800

    Add logic and styles for pending chip

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -26,6 +26,12 @@ function Game(args) {
   ];
   // The current player is null when a game is not in progress
   this.currentPlayer = null;
+  // Whether or not the game is in progress
+  this.gameInProgress = false;
+  // The chip above the grid that is about to be placed
+  this.pendingChip = new Chip({player: new Player({color: 'red'})});
+  // The chip that was most recently placed in the board
+  this.lastInsertedChip = null;
 }
 
 function Grid(args) {
@@ -52,9 +58,31 @@ function Player(args) {
 }
 
 var GridComponent = {};
+GridComponent.controller = function (game) {
+  return {
+    // Translate the pending chip to be aligned with whatever the user hovered
+    // over (which is guaranteed to be either a chip, placeholder chip, or grid
+    // column)
+    getPendingChipTranslate: function (game, event) {
+      if (game.pendingChip) {
+        var pendingChipElem = document.querySelector('.chip.pending');
+        var gridElem = event.currentTarget;
+        // Ensure that the left margin of a chip or placeholder chip is included in the offset measurement
+        var marginLeft = parseInt(window.getComputedStyle(event.target)['margin-left']);
+        var offsetX = event.target.offsetLeft - marginLeft;
+        pendingChipElem.style.transform = 'translate(' + offsetX + 'px,0)';
+      }
+    }
+  };
+};
 GridComponent.view = function (ctrl, game) {
   var grid = game.grid;
-  return m('div', {id: 'grid'}, [
+  return m('div', {id: 'grid', onmousemove: _.partial(ctrl.getPendingChipTranslate, game)}, [
+    // Area where to-be-placed chips are dropped from
+    m('div', {id: 'pending-chip-zone'}, game.pendingChip ?
+      m('div', {
+        class: ['chip', 'pending', game.pendingChip.player.color].join(' ')
+      }) : null),
     // Bottom grid of chip placeholders (indicating space chips can occupy)
     m('div', {id: 'chip-placeholders'}, _.times(grid.columnCount, function (c) {
       return m('div', {class: 'grid-column'}, _.times(grid.rowCount, function (r) {
