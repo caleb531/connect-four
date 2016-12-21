8dca36c371299f3f4f0be4016be664e2a9c2926a
Caleb Evans <caleb@calebevans.me>
2016-12-20 17:54:27 -0800

    Refactor grid creation code to be smaller

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,62 +4,54 @@ var GameComponent = {
   controller: function () {},
   view: function (ctrl) {
     return [
-      m(GameGridComponent, {
-        columnCount: 7,
-        rowCount: 6,
-        chipSize: 50,
-        chipMargin: 6
-      })
+      m(GameGridComponent, new GameGrid({
+          columnCount: 7,
+          rowCount: 6,
+          chipSize: 50,
+          chipMargin: 6
+        })
+      )
     ];
   }
 };
 
+function GameGrid(args) {
+  this.columnCount = args.columnCount;
+  this.rowCount = args.rowCount;
+  this.chipSize = args.chipSize;
+  this.chipMargin = args.chipMargin;
+}
+
 var GameGridComponent = {
   controller: function () {
     return {
-      getStyleStr: function (styles) {
-        var declarations = [];
-        for (var propName in styles) {
-          if (styles.hasOwnProperty(propName)) {
-            declarations.push(propName + ':' + styles[propName]);
-          }
-        }
-        return declarations.join(';');
-      },
-      getGridStyle: function (columnCount, rowCount, chipSize, chipMargin) {
-        var gridWidth = columnCount * (chipSize + (chipMargin * 2));
-        var gridHeight = rowCount * (chipSize + (chipMargin * 2));
-        return this.getStyleStr({
+      getGridStyle: function (grid) {
+        var gridWidth = grid.columnCount * (grid.chipSize + (grid.chipMargin * 2));
+        var gridHeight = grid.rowCount * (grid.chipSize + (grid.chipMargin * 2));
+        return {
           width: gridWidth + 'px',
           height: gridHeight + 'px'
-        });
+        };
       },
-      getChipStyle: function (c, r, chipSize, chipMargin) {
-        var chipX = c * (chipSize + (chipMargin * 2)) + chipMargin;
-        var chipY = r * (chipSize + (chipMargin * 2)) + chipMargin;
-        return this.getStyleStr({
-          width: chipSize + 'px',
-          height: chipSize + 'px',
+      getChipStyle: function (c, r, grid) {
+        var chipX = c * (grid.chipSize + (grid.chipMargin * 2)) + grid.chipMargin;
+        var chipY = r * (grid.chipSize + (grid.chipMargin * 2)) + grid.chipMargin;
+        return {
+          width: grid.chipSize + 'px',
+          height: grid.chipSize + 'px',
           transform: 'translate(' + chipX + 'px,' + chipY + 'px)'
-        });
-      },
-      count: function (start, end, fn) {
-        var items = [];
-        for (var i = start; i < end; i += 1) {
-          items.push(fn(i));
-        }
-        return items;
+        };
       }
     };
   },
-  view: function (ctrl, args) {
-    return m('div', {id: 'grid', style: ctrl.getGridStyle(args.columnCount, args.rowCount, args.chipSize, args.chipMargin)},
-      ctrl.count(0, args.columnCount, function (c) {
-        return m('div', {class: 'column'}, ctrl.count(0, args.rowCount, function (r) {
+  view: function (ctrl, grid) {
+    return m('div', {id: 'grid', style: ctrl.getGridStyle(grid)},
+      _.times(grid.columnCount, function (c) {
+        return m('div', {class: 'column'}, _.times(grid.rowCount, function (r) {
           return m('div', {
             key: 'chip-' + c + '-' + r,
             class: 'chip-placeholder',
-            style: ctrl.getChipStyle(c, r, args.chipSize, args.chipMargin)
+            style: ctrl.getChipStyle(c, r, grid)
           });
         }));
       })
