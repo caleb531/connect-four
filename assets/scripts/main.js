43d718da9c1e35799cfff9cb1eb422f5dd661463
Caleb Evans <caleb@calebevans.me>
2016-12-20 19:29:45 -0800

    Define component methods outside object literals
    This reduces the indentation by one level across most of the script.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,18 +1,16 @@
 (function() {
 
-var GameComponent = {
-  controller: function () {},
-  view: function (ctrl) {
-    return [
-      m(GridComponent, new Grid({
-          columnCount: 7,
-          rowCount: 6,
-          chipSize: 50,
-          chipMargin: 6
-        })
-      )
-    ];
-  }
+var GameComponent = {};
+GameComponent.view = function () {
+  return [
+    m(GridComponent, new Grid({
+        columnCount: 7,
+        rowCount: 6,
+        chipSize: 50,
+        chipMargin: 6
+      })
+    )
+  ];
 };
 
 function Grid(args) {
@@ -32,40 +30,39 @@ Grid.prototype.resetGrid = function () {
   }
 };
 
-var GridComponent = {
-  controller: function () {
-    return {
-      getGridStyle: function (grid) {
-        var gridWidth = grid.columnCount * (grid.chipSize + (grid.chipMargin * 2));
-        var gridHeight = grid.rowCount * (grid.chipSize + (grid.chipMargin * 2));
-        return {
-          width: gridWidth + 'px',
-          height: gridHeight + 'px'
-        };
-      },
-      getChipStyle: function (c, r, grid) {
-        var chipX = c * (grid.chipSize + (grid.chipMargin * 2)) + grid.chipMargin;
-        var chipY = r * (grid.chipSize + (grid.chipMargin * 2)) + grid.chipMargin;
-        return {
-          width: grid.chipSize + 'px',
-          height: grid.chipSize + 'px',
-          transform: 'translate(' + chipX + 'px,' + chipY + 'px)'
-        };
-      }
-    };
-  },
-  view: function (ctrl, grid) {
-    return m('div', {id: 'grid', style: ctrl.getGridStyle(grid)},
-      _.times(grid.columnCount, function (c) {
-        return m('div', {class: 'column'}, _.times(grid.rowCount, function (r) {
-          return m('div', {
-            class: 'chip-placeholder',
-            style: ctrl.getChipStyle(c, r, grid)
-          });
-        }));
-      })
-    );
-  }
+var GridComponent = {};
+GridComponent.controller = function () {
+  return {
+    getGridStyle: function (grid) {
+      var gridWidth = grid.columnCount * (grid.chipSize + (grid.chipMargin * 2));
+      var gridHeight = grid.rowCount * (grid.chipSize + (grid.chipMargin * 2));
+      return {
+        width: gridWidth + 'px',
+        height: gridHeight + 'px'
+      };
+    },
+    getChipStyle: function (c, r, grid) {
+      var chipX = c * (grid.chipSize + (grid.chipMargin * 2)) + grid.chipMargin;
+      var chipY = r * (grid.chipSize + (grid.chipMargin * 2)) + grid.chipMargin;
+      return {
+        width: grid.chipSize + 'px',
+        height: grid.chipSize + 'px',
+        transform: 'translate(' + chipX + 'px,' + chipY + 'px)'
+      };
+    }
+  };
+};
+GridComponent.view = function (ctrl, grid) {
+  return m('div', {id: 'grid', style: ctrl.getGridStyle(grid)},
+    _.times(grid.columnCount, function (c) {
+      return m('div', {class: 'column'}, _.times(grid.rowCount, function (r) {
+        return m('div', {
+          class: 'chip-placeholder',
+          style: ctrl.getChipStyle(c, r, grid)
+        });
+      }));
+    })
+  );
 };
 
 m.mount(document.getElementById('game'), GameComponent);
