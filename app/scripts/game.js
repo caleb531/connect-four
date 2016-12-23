2d02f278958147a462e26c64a27a16716a81154b
Caleb Evans <caleb@calebevans.me>
2016-12-23 10:21:38 -0800

    Properly end game when player gets connect-four

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -13,22 +13,33 @@ function Game(args) {
   // The current player is null when a game is not in progress
   this.currentPlayer = null;
   // Whether or not the game is in progress
-  this.gameInProgress = false;
+  this.inProgress = false;
   // The chip above the grid that is about to be placed
   this.pendingChip = null;
   // The chip that was most recently placed in the board
   this.lastPlacedChip = null;
-  // TODO: remove this when P2 mode testing is finished
-  this.startGame();
+  // The winning player of the game
+  this.winner = null;
 }
 
 Game.prototype.startGame = function (args) {
-  // If 2-player game is selected, assume two human players
-  this.players.push(new Player({color: 'red', playerNum: 1}));
-  this.players.push(new Player({color: 'blue', playerNum: 2}));
-  this.gameInProgress = true;
+  // Create new players if there are none
+  if (this.players.length === 0) {
+    this.players.push(
+        new Player({color: 'red', playerNum: 1}),
+        new Player({color: 'blue', playerNum: 2})
+    );
+  }
   this.currentPlayer = this.players[0];
+  this.inProgress = true;
   this.startTurn();
+};
+
+// End the game without resetting the grid
+Game.prototype.endGame = function () {
+  this.inProgress = false;
+  this.currentPlayer = null;
+  this.pendingChip = null;
 };
 
 // Start the turn of the current player
@@ -38,13 +49,15 @@ Game.prototype.startTurn = function () {
 
 // End the turn of the current player and switch to the next player
 Game.prototype.endTurn = function () {
-  // Switch to next player
-  if (this.currentPlayer === this.players[0]) {
-    this.currentPlayer = this.players[1];
-  } else {
-    this.currentPlayer = this.players[0];
+  if (this.inProgress) {
+    // Switch to next player's turn
+    if (this.currentPlayer === this.players[0]) {
+      this.currentPlayer = this.players[1];
+    } else {
+      this.currentPlayer = this.players[0];
+    }
+    this.startTurn();
   }
-  this.startTurn();
 };
 
 // Return the index of the next available row for the given column
@@ -60,11 +73,13 @@ Game.prototype.getNextAvailableSlot = function (args) {
 
 // Insert the current pending chip into the columns array at the given index
 Game.prototype.placePendingChip = function (args) {
-  this.grid.columns[args.column].push(this.pendingChip);
-  this.lastPlacedChip = this.pendingChip;
-  this.lastPlacedChip.column = args.column;
-  this.lastPlacedChip.row = this.grid.columns[args.column].length - 1;
-  this.pendingChip = null;
+  if (this.pendingChip) {
+    this.grid.columns[args.column].push(this.pendingChip);
+    this.lastPlacedChip = this.pendingChip;
+    this.lastPlacedChip.column = args.column;
+    this.lastPlacedChip.row = this.grid.columns[args.column].length - 1;
+    this.pendingChip = null;
+  }
 };
 
 // Find same-color neighbors connected to the given chip in the given direction
@@ -110,13 +125,14 @@ Game.prototype.checkForWinner = function () {
     }));
     // If four connected same-color chips are found, mark them as highlighted
     if (connectedChips.length === 4) {
+      game.winner = game.lastPlacedChip.player;
       _.forEach(connectedChips, function (neighbor) {
         neighbor.highlighted = true;
       });
-      return game.lastPlacedChip.player;
+      game.endGame();
+      return;
     }
   });
-  return null;
 };
 // The relative directions to check when checking for connected chip neighbors
 Game.connectionDirections = [
@@ -126,12 +142,11 @@ Game.connectionDirections = [
   {x: 1, y: 1} // Top-right
 ];
 
+// Reset the game and grid completely without starting a new game
 Game.prototype.resetGame = function (args) {
-  this.gameInProgress = false;
-  this.players.length = 0;
-  this.currentPlayer = null;
-  this.pendingChip = null;
+  this.endGame();
   this.lastPlacedChip = null;
+  this.winner = null;
   this.grid.resetGrid();
 };
 
