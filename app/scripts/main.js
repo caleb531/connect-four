2a8fe5705d88dd426efb0135a89a5396e639b362
Caleb Evans <caleb@calebevans.me>
2016-12-23 12:10:09 -0800

    Add FastClick for a smoother mobile experience

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var m = require('mithril');
+var attachFastClick = require('fastclick');
 var Game = require('./game');
 
 m.mount(document.getElementById('game'), Game.Component);
+attachFastClick(document.body);
