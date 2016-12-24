76a65c6b0a5fd5e145ad87d0be89a0ab02429733
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2016-12-23 20:15:04 -0800

    Use if statements rather than logical operators

--- app/scripts/browser.js
+++ app/scripts/browser.js
@@ -7,9 +7,15 @@ var Browser = {};
 
 // Map of CSS properties to property names used by the DOM in this browser.
 var normalizedProperties = {
-  'transform': _.find(['transform', 'WebkitTransform'], function (property) {
-    return document.documentElement.style[property] !== undefined;
-  }) || 'transform'
+  'transform': (function () {
+    var property = _.find(['transform', 'WebkitTransform'], function (property) {
+      return document.documentElement.style[property] !== undefined;
+    });
+    if (property) {
+      return property;
+    }
+    return 'transform';
+  }())
 };
 
 // Convert CSS properties and values to equivalent properties and values for
@@ -35,7 +41,10 @@ var normalizedEventNames = {
     ], function (pair) {
       return window[pair.property] !== undefined;
     });
-    return (pair && pair.name) || 'transitionend';
+    if (pair) {
+      return pair.name;
+    }
+    return 'transitionend';
   }())
 };
 
