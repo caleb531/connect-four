2e71ee747377116706991e18199626ab1a3466e9
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2016-12-24 11:54:30 -0800

    Refactor Game test to use simplified API

--- test/game.test.js
+++ test/game.test.js
@@ -7,35 +7,21 @@ var Game = require('../app/scripts/game');
 describe('Game', function () {
 
   it('should determine a winner', function () {
-    var game = new Game({
-      grid: new Grid({
-        columnCount: 7,
-        rowCount: 6
-      })
-    });
+    var game = new Game();
     game.startGame();
     game.placePendingChip({column: 0}); // P1
-    game.endTurn();
 
     // Winners haven't won until they've won.
-    game.checkForWin();
     expect(game.winner).to.be.null;
 
     // Play an extremely skillful game.
     game.placePendingChip({column: 1}); // P2
-    game.endTurn();
     game.placePendingChip({column: 0}); // P1
-    game.endTurn();
     game.placePendingChip({column: 1}); // P2
-    game.endTurn();
     game.placePendingChip({column: 0}); // P1
-    game.endTurn();
     game.placePendingChip({column: 1}); // P2
-    game.endTurn();
     game.placePendingChip({column: 0}); // P1
-    game.endTurn();
 
-    game.checkForWin();
     expect(game.winner).not.to.be.null;
     expect(game.winner).to.have.property('name', 'Player 1');
   });
