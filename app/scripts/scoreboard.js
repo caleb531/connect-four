ca6fb2018ed3d2bd0c5642abe0fb4d905e2efefe
Caleb Evans <caleb@calebevans.me>
2016-12-23 16:03:03 -0800

    Style player stats to be the color of the respective player

--- app/scripts/scoreboard.js
+++ app/scripts/scoreboard.js
@@ -2,13 +2,14 @@
 
 var m = require('mithril');
 var _ = require('underscore');
+var classNames = require('classnames');
 
 var Scoreboard = {};
 Scoreboard.Component = {};
 
 Scoreboard.Component.view = function (ctrl, game) {
   return m('div#game-scoreboard', game.players.map(function (player) {
-    return m('div.player-stats', [
+    return m('div.player-stats', {class: classNames(player.color)}, [
       m('div.player-name', player.name),
       m('div.player-score', player.score)
     ]);
