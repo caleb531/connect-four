4bf2e0cf652b1a64f37b211a3d0496c2eacd6f56
Caleb Evans <caleb@calebevans.me>
2016-12-26 18:18:03 -0800

    Add tests for game initialization

--- test/game.spec.js
+++ test/game.spec.js
@@ -2,9 +2,40 @@
 
 var expect = require('chai').expect;
 var Grid = require('../app/scripts/grid');
+var Player = require('../app/scripts/player');
 var Game = require('../app/scripts/game');
 
 describe('Game', function () {
+
+  it('should initialize game with no arguments', function () {
+    var game = new Game();
+    expect(game.grid.columnCount).to.equal(7);
+    expect(game.grid.rowCount).to.equal(6);
+    expect(game.players.length).to.equal(2);
+    expect(game.currentPlayer).to.be.null;
+    expect(game.pendingChip).to.be.null;
+    expect(game.lastPlacedChip).to.be.null;
+    expect(game.winner).to.be.null;
+  });
+
+  it('should initialize game with arguments', function () {
+    var game = new Game({
+      players: [
+        new Player({color: 'blue', name: 'Bob'}),
+        new Player({color: 'black', name: 'Larry'})
+      ],
+      grid: new Grid({columnCount: 9, rowCount: 8})
+    });
+    expect(game.grid.columnCount).to.equal(9);
+    expect(game.grid.rowCount).to.equal(8);
+    expect(game.players.length).to.equal(2);
+    expect(game.players[0].name).to.equal('Bob');
+    expect(game.players[1].name).to.equal('Larry');
+    expect(game.currentPlayer).to.be.null;
+    expect(game.pendingChip).to.be.null;
+    expect(game.lastPlacedChip).to.be.null;
+    expect(game.winner).to.be.null;
+  });
 
   it('should determine a winner', function () {
     var game = new Game();
