b1164388a59aa47d6c8ed2068227101656edb1db
Caleb Evans <caleb@calebevans.me>
2017-01-04 17:49:08 -0800

    Add configuration for building with Travis

--- karma.conf.js
+++ karma.conf.js
@@ -5,11 +5,17 @@
 module.exports = function (config) {
   config.set({
     basePath: 'public',
-    browsers: ['Chrome'],
+    browsers: [process.env.TRAVIS ? 'Chrome_travis_ci' : 'Chrome'],
     files: ['scripts/modules.js', 'scripts/test.js'],
     frameworks: ['mocha'],
     preprocessors: {
       '**/*.js': ['sourcemap']
+    },
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
     }
   });
 };
