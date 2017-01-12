14864695744901b6479ad44f1d028a5b449b8448
Caleb Evans <caleb@calebevans.me>
2017-01-12 14:38:19 -0800

    Add commented failing AI tests
    Both of these tests currently fail with the current AI algorithm, but
    they're useful tests to have and may be uncommented later if the
    algorithm is revised again.

--- test/ai-player.spec.js
+++ test/ai-player.spec.js
@@ -120,6 +120,28 @@ describe('AI player', function () {
     expect(game.players[1].computeNextMove(game)).to.equal(5);
   });
 
+  // it('should block diagonal opponent win (#3)', function () {
+  //   var game = new Game();
+  //   game.setPlayers(1);
+  //   game.startGame();
+  //   placeChips({
+  //     game: game,
+  //     columns: [2, 0, 3, 1, 4, 5, 1, 0, 2, 4, 3, 0, 5, 1, 3, 2, 4, 2, 0, 3, 1, 0, 3, 2, 2, 3, 4]
+  //   });
+  //   expect(game.players[1].computeNextMove(game)).to.equal(5);
+  // });
+
+  // it('should block one win of opponent double-win', function () {
+  //   var game = new Game();
+  //   game.setPlayers(1);
+  //   game.startGame();
+  //   placeChips({
+  //     game: game,
+  //     columns: [3, 0, 2, 1, 3, 0, 3, 3, 0, 0, 2, 1, 2]
+  //   });
+  //   expect(game.players[1].computeNextMove(game)).to.be.oneOf([1, 2]);
+  // });
+
   it('should win horizontally on turn', function () {
     var game = new Game();
     game.setPlayers(1);
