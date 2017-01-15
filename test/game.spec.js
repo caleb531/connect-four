09836de8650d539ee3a608e9eb03571c85d5768c
Caleb Evans <caleb@calebevans.me>
2017-01-14 20:16:34 -0800

    Add tests for setting game players

--- test/game.spec.js
+++ test/game.spec.js
@@ -48,11 +48,89 @@ describe('game', function () {
     expect(game).to.have.property('winner', null);
   });
 
+  it('should initialize 1P game', function () {
+    var game = new Game();
+    game.setPlayers(1);
+    expect(game.players).to.have.length(2);
+    expect(game.players[0].type).to.equal('human');
+    expect(game.players[1].type).to.equal('ai');
+  });
+
+  it('should initialize 2P game', function () {
+    var game = new Game();
+    game.setPlayers(2);
+    expect(game.players).to.have.length(2);
+    expect(game.players[0].type).to.equal('human');
+    expect(game.players[1].type).to.equal('human');
+  });
+
+  it('should preserve players when continuing in 1P mode', function () {
+    var game = new Game();
+    game.setPlayers(1);
+    game.players[0].score = 10;
+    game.players[1].score = 20;
+    game.setPlayers(1);
+    expect(game.players).to.have.length(2);
+    expect(game.players[0].type).to.equal('human');
+    expect(game.players[0].score).to.equal(10);
+    expect(game.players[1].type).to.equal('ai');
+    expect(game.players[1].score).to.equal(20);
+  });
+
+  it('should preserve players when continuing in 2P mode', function () {
+    var game = new Game();
+    game.setPlayers(2);
+    game.players[0].score = 12;
+    game.players[1].score = 16;
+    game.setPlayers(2);
+    expect(game.players).to.have.length(2);
+    expect(game.players[0].type).to.equal('human');
+    expect(game.players[0].score).to.equal(12);
+    expect(game.players[1].type).to.equal('human');
+    expect(game.players[1].score).to.equal(16);
+  });
+
+  it('should initialize new players when switching from 1P to 2P', function () {
+    var game = new Game();
+    game.setPlayers(1);
+    game.players[0].score = 10;
+    game.players[1].score = 15;
+    game.setPlayers(2);
+    expect(game.players).to.have.length(2);
+    expect(game.players[0].type).to.equal('human');
+    expect(game.players[0].score).to.equal(0);
+    expect(game.players[1].type).to.equal('human');
+    expect(game.players[1].score).to.equal(0);
+  });
+
+  it('should initialize new players when switching from 2P to 1P', function () {
+    var game = new Game();
+    game.setPlayers(2);
+    game.players[0].score = 10;
+    game.players[1].score = 15;
+    game.setPlayers(1);
+    expect(game.players).to.have.length(2);
+    expect(game.players[0].type).to.equal('human');
+    expect(game.players[0].score).to.equal(0);
+    expect(game.players[1].type).to.equal('ai');
+    expect(game.players[1].score).to.equal(0);
+  });
+
   it('should start', function () {
     var game = new Game();
     game.setPlayers(2);
     game.startGame();
     expect(game.currentPlayer).to.equal(game.players[0]);
+    expect(game.inProgress).to.be.true;
+  });
+
+  it('should set starting player when starting', function () {
+    var game = new Game();
+    game.setPlayers(2);
+    game.startGame({
+      startingPlayer: game.players[1]
+    });
+    expect(game.currentPlayer).to.equal(game.players[1]);
     expect(game.inProgress).to.be.true;
   });
 
