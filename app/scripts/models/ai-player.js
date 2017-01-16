bd27ccdadb81e5170b7d1a137a1b9ca55cfc1e11
Caleb Evans <caleb@calebevans.me>
2017-01-15 21:28:27 -0800

    Revert "Choose random column as default"
    This reverts commit d8a90d9e59e39d9bbb238cba1e992d367488c7ec.

--- app/scripts/models/ai-player.js
+++ app/scripts/models/ai-player.js
@@ -29,10 +29,10 @@ AIPlayer.prototype.computeNextMove = function (game) {
   var bestMove = this.maximizeMove(
     game.grid, game.getOtherPlayer(this), AIPlayer.maxComputeDepth,
     Grid.minScore, Grid.maxScore);
-  // If no particular column yields an advantage or disadvantage, choose a
-  // column at random
+  // If no particular column yields an advantage or disadvantage, default to the
+  // center column
   if (bestMove.column === 0 && bestMove.score === 0) {
-    bestMove.column = Math.floor(Math.random() * game.grid.columnCount);
+    bestMove.column = 3;
   }
   // Choose next available column if original pick is full
   while (game.grid.getNextAvailableSlot({column: bestMove.column}) === null) {
