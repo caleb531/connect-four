fa5008a77ad74591c6b1961a47ce45aee46d5b14
Caleb Evans <caleb@calebevans.me>
2017-03-09 12:08:01 -0800

    Switch from karma to mocha exclusively for testing
    Switching from karma to mocha yields a number of benefits:
      - An instance of Chrome no longer needs to be spawned to run the
        tests, since the tests only cover the app's models, which do not
        interact with the DOM
      - The Brunch configuration becomes simpler and roughly 57% smaller
        because there is no longer any need to generate a separate
        modules.js, nor is there a need to maintain a separate
        test/main.js
      - Brunch compilation becomes faster because Brunch no longer needs
        to compile tests; in effect, the number of compiled files has
        dropped from 50 to 16
      - Because karma and its related devDependencies are no longer used,
        they can be removed from the project, thereby reducing the runtime
        of `npm install`

--- brunch-config.js
+++ brunch-config.js
@@ -2,19 +2,11 @@
 // See http://brunch.io for documentation.
 'use strict';
 
-var modules = ['app/scripts/**/*.js', /^node_modules/];
 module.exports = {
   files: {
     javascripts: {
       joinTo: {
-        // Create two scripts, each containing application modules.  One has
-        // initialization code (main) autoRequire'd, and the other doesn't.  All
-        // of the application is loaded in a file (CSP-friendly) and in only one
-        // file (latency-friendly) in the browser, and the modules are tested in
-        // isolation in unit tests.
-        'scripts/main.js': modules,
-        'scripts/modules.js': modules,
-        'scripts/test.js': ['test/*.js']
+        'scripts/main.js': ['app/scripts/**/*.js', /^node_modules/]
       }
     },
     stylesheets: {
@@ -25,14 +17,12 @@ module.exports = {
   },
   modules: {
     autoRequire: {
-      'scripts/main.js': ['app/scripts/main'],
-      'scripts/test.js': ['test/main']
-    },
-    nameCleaner: function (path) {
-      // Don't strip "app/" from module paths to ensure ability to require.
-      // https://github.com/brunch/brunch/issues/1441#issuecomment-241268612
-      return path;
+      'scripts/main.js': ['scripts/main']
     }
+  },
+  paths: {
+    // Exclude test files from compilation
+    watched: ['app']
   },
   plugins: {
     postcss: {
@@ -43,13 +33,4 @@ module.exports = {
       ]
     }
   },
-  sourceMaps: 'inline', // For karma-sourcemap-loader.
-  overrides: {
-    production: {
-      paths: {
-        // Exclude files in "test" from compilation.
-        watched: ['app', 'vendor']
-      }
-    }
-  }
 };
--- test/main.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// Run all test files that the application knows about.
-window.require.list().filter(function (moduleName) {
-  return (/^test\/.*?\.spec\.js$/).test(moduleName);
-}).forEach(require);
