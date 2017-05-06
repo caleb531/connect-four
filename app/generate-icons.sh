f23b627e08126da137d95b50e2ac04ba3fa3590c
Caleb Evans <caleb@calebevans.me>
2017-05-06 12:48:46 -0700

    Revert "Remove icon generation script"
    This reverts commit 1a5d39035ff18b1fd418908c5a370331e1e4148a.

---
+++ app/generate-icons.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Do not proceed if rsvg-convert is not available
+if ! type rsvg-convert &> /dev/null; then
+  >&2 echo 'librsvg not installed (rsvg-convert not found in PATH)'
+  exit
+fi
+
+# The path to the directory where all icons reside
+ICON_DIR=app/assets/icons
+# The path to the SVG from which icon sizes are generated
+SVG_PATH="$ICON_DIR"/app-icon.svg
+# The path to the directory where the PNG icons are generated
+PNG_DIR="$ICON_DIR"/png
+# The sizes to generate (all icons are square)
+PNG_SIZES=(32 180 192 256 384 512)
+
+mkdir -p "$PNG_DIR"
+for size in ${PNG_SIZES[@]}; do
+  rsvg-convert \
+    --format png \
+    --width "$size" \
+    "$SVG_PATH" \
+    --output "$PNG_DIR"/app-icon-"$size".png
+done
