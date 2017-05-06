caleb531/connect-four

commit b9b55ad4e6b0c5cde54c884e8a64ce48f267729f

Streamline icon generation setup
- Generate icons in public/ instead of app/assets/icons/png, as the
  former is already gitignore'd anyway
- Do not build original SVG app icon with project, as this is never
  served
- Move icon generation script into app/icons alongside SVG (as this is a
  better home for the script than app/)
- Do not cache generated icons other than the favicon, as those icons
  will never be served in-app
- Build project before generating icons when running npm script

brunch-config.js
@@ -46,7 +46,13 @@ module.exports = {
       swFileName: 'service-worker.js',
       options: {
         cacheId: 'connect-four',
-        staticFileGlobs: ['public/**/!(*map*)'],
+        staticFileGlobs: [
+          'public/index.html',
+          'public/styles/main.css',
+          'public/scripts/main.js',
+          'public/icons/favicon.png',
+          'public/fonts'
+        ],
         stripPrefix: 'public',
         replacePrefix: '/connect-four'
       }

app/assets/index.html
@@ -8,8 +8,8 @@
   <meta name="theme-color" content="#ffffff" />
 
   <link rel="stylesheet" href="styles/main.css" />
-  <link rel="shortcut icon" href="icons/png/app-icon-32.png" />
-  <link rel="apple-touch-icon" href="icons/png/app-icon-180.png" sizes="180x180" />
+  <link rel="shortcut icon" href="icons/favicon.png" />
+  <link rel="apple-touch-icon" href="icons/app-icon-180.png" sizes="180x180" />
   <link rel="manifest" href="manifest.json" />
 </head>
 <body>

app/icons/generate-icons.sh
@@ -6,16 +6,24 @@ if ! type rsvg-convert &> /dev/null; then
   exit
 fi
 
-# The path to the directory where all icons reside
-ICON_DIR=app/assets/icons
 # The path to the SVG from which icon sizes are generated
-SVG_PATH="$ICON_DIR"/app-icon.svg
+SVG_PATH=app/icons/app-icon.svg
 # The path to the directory where the PNG icons are generated
-PNG_DIR="$ICON_DIR"/png
+PNG_DIR=public/icons
 # The sizes to generate (all icons are square)
-PNG_SIZES=(32 180 192 256 384 512)
+PNG_SIZES=(180 192 256 384 512)
 
+# Create destination directory if it does not exist
 mkdir -p "$PNG_DIR"
+
+# Generate favicon separately for the sake of the filename
+rsvg-convert \
+  --format png \
+  --width 32 \
+  "$SVG_PATH" \
+  --output "$PNG_DIR"/favicon.png
+
+# Generate icon sizes for web app manifest
 for size in ${PNG_SIZES[@]}; do
   rsvg-convert \
     --format png \

commit 3b00d6836c0c40e18f778e16d0da35a8685cc5b8

Fix bug where fonts were not properly cached
This bug is caused by a regression in b9b55ad. The staticFileGlobs items
are expected to be file glob patterns, and not actual directories.

brunch-config.js
@@ -51,7 +51,7 @@ module.exports = {
           'public/styles/main.css',
           'public/scripts/main.js',
           'public/icons/favicon.png',
-          'public/fonts'
+          'public/fonts/*.*'
         ],
         stripPrefix: 'public',
         replacePrefix: '/connect-four'