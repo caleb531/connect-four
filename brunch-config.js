e44869a361937e7f8e220db9ca75fdd8e9000a27
Caleb Evans <caleb@calebevans.me>
2017-05-08 16:40:10 -0700

    Remove app manifest from service worker cache
    The manifest does not need to be cached. See:
    <https://twitter.com/paul_irish/status/861726666128728064>

--- brunch-config.js
+++ brunch-config.js
@@ -62,7 +62,6 @@ module.exports = {
         cacheId: 'connect-four',
         staticFileGlobs: [
           'public/index.html',
-          'public/manifest.json',
           'public/styles/main.css',
           'public/scripts/main.js',
           'public/icons/favicon.png',
