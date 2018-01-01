6693a1df20633b6bdc1034f7ea9cee595bc97fea
Caleb Evans <caleb@calebevans.me>
2018-01-01 13:49:56 -0800

    Convert human player model to ES6 class

--- app/scripts/models/human-player.js
+++ app/scripts/models/human-player.js
@@ -2,10 +2,9 @@ import Player from './player';
 
 // A human player that requires user interaction; every human player inherits
 // from the base Player model
-function HumanPlayer(args) {
-  Player.call(this, args);
+class HumanPlayer extends Player {
+  // Nothing to do here!
 }
-HumanPlayer.prototype = Object.create(Player.prototype);
 HumanPlayer.prototype.type = 'human';
 
 export default HumanPlayer;
