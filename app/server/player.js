c95b1defa184aba688d207496626ac89ce061888
Caleb Evans <caleb@calebevans.me>
2019-06-10 21:30:31 -0700

    Store player color on server

--- app/server/player.js
+++ app/server/player.js
@@ -1,9 +1,13 @@
 class Player {
 
-  constructor({ socket }) {
-    // The identifier for this player, unique on a per-room level
-    this.id = Date.now();
-    // The current socket assocuat
+  /* eslint-disable no-shadow */
+  constructor({ name, color, socket }) {
+    // The name of the player as entered by the user
+    this.name = name;
+    // The user-defined color of the user as defined by the client
+    this.color = color;
+    // The current socket associated with this player; this will be updated by
+    // the server when the socket is disconnected
     this.socket = socket;
     // The player's total number of wins across all games
     this.score = 0;
