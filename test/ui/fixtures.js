76b5d0b37b9601e6d58c16cc591750f21bbc6575
Caleb Evans <caleb@calebevans.me>
2019-06-14 20:56:14 -0700

    Export functions inline

--- test/ui/fixtures.js
+++ test/ui/fixtures.js
@@ -4,19 +4,17 @@ import { qs } from './utils.js';
 
 // Minimize the transition duration to speed up tests (interestingly, a
 // duration of 0ms will prevent transitionEnd from firing)
-function _before() {
+export function _before() {
   let style = document.createElement('style');
   style.innerHTML = '* {transition-duration: 200ms !important;}';
   document.head.appendChild(style);
 }
 
-function _beforeEach() {
+export function _beforeEach() {
   document.body.appendChild(document.createElement('main'));
   m.mount(qs('main'), AppComponent);
 }
 
-function _afterEach() {
+export function _afterEach() {
   m.mount(qs('main'), null);
 }
-
-export { _before, _beforeEach, _afterEach };
--- test/ui/utils.js
+++ test/ui/utils.js
@@ -1,14 +1,14 @@
-function qs(selector) {
+export function qs(selector) {
   return document.querySelector(selector);
 }
 
-function qsa(selector) {
+export function qsa(selector) {
   return document.querySelectorAll(selector);
 }
 
 // Wait for the next transition on the given element to complete, timing out
 // and erroring if the transition never completes
-function onPendingChipTransitionEnd() {
+export function onPendingChipTransitionEnd() {
   return new Promise(function (resolve) {
     let pendingChip = qs('.chip.pending');
     pendingChip.addEventListener('transitionend', function transitionend() {
@@ -20,12 +20,9 @@ function onPendingChipTransitionEnd() {
 
 // Simulate a mouse event at the specified coordinates, relative to the given
 // element
-function triggerMouseEvent(elem, eventType, x, y) {
+export function triggerMouseEvent(elem, eventType, x, y) {
   elem.dispatchEvent(new MouseEvent(eventType, {
     clientX: elem.offsetLeft + x,
     clientY: elem.offsetTop + y
   }));
 }
-
-export { qs, qsa };
-export { onPendingChipTransitionEnd, triggerMouseEvent };
