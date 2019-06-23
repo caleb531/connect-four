6244dbc21b9f75c2bb3daa97fb0fd020974caa3d
Caleb Evans <caleb@calebevans.me>
2019-06-23 15:17:38 -0700

    Underline current player's name in scoreboard

--- app/scripts/components/scoreboard.js
+++ app/scripts/components/scoreboard.js
@@ -1,4 +1,5 @@
 import m from 'mithril';
+import classNames from '../classnames.js';
 
 // The area of the game UI that displays each player's current score
 class ScoreboardComponent {
@@ -9,7 +10,9 @@ class ScoreboardComponent {
 
   view() {
     return m('div#game-scoreboard', this.game.players.map((player) => {
-      return m(`div.player-stats.${player.color}`, [
+      return m(`div.player-stats.${player.color}`, {
+        class: classNames({ 'current-player': player === this.game.currentPlayer })
+      }, [
         m('div.player-name', player.name),
         m('div.player-score', player.score)
       ]);
