6642a38b7fb5fc486cd5242bff840bc8f81b4d47
Caleb Evans <caleb@calebevans.me>
2019-06-26 12:37:29 -0700

    Split out, add more restoreFromServer tests

--- test/game/restore-from-server.spec.js
+++ test/game/restore-from-server.spec.js
@@ -1,51 +1,68 @@
 import Game from '../../app/scripts/models/game.js';
-import Player from '../../app/scripts/models/player.js';
+import HumanPlayer from '../../app/scripts/models/human-player.js';
+import OnlinePlayer from '../../app/scripts/models/online-player.js';
 import Chip from '../../app/scripts/models/chip.js';
 
 describe('game', function () {
 
   let session;
+  let game;
+  let serverGame;
+  let localPlayer;
 
   beforeEach(function () {
      session = { on: () => {/* this is a noop */} };
+     game = new Game({ session });
+     serverGame = {
+       inProgress: true,
+       players: [
+         { name: 'Abott', color: 'red' },
+         { name: 'Costello', color: 'blue' }
+       ],
+       currentPlayer: 'blue',
+       requestingPlayer: 'red',
+       grid: {
+         columnCount: 7,
+         rowCount: 6,
+         columns: [
+           [], [], [], [], [],
+           [
+             { column: 5, row: 0, player: 'blue' },
+             { column: 5, row: 1, player: 'red' }
+           ],
+           []
+         ],
+         lastPlacedChip: { column: 5, row: 1, player: 'red' }
+       }
+     };
+     localPlayer = serverGame.players[0];
   });
 
-  it('restore state from server', function () {
-    let game = new Game({
-      session
-    });
-    let serverGame = {
-      inProgress: true,
-      players: [
-        { name: 'Abott', color: 'red' },
-        { name: 'Costello', color: 'blue' }
-      ],
-      currentPlayer: 'blue',
-      requestingPlayer: 'red',
-      grid: {
-        columnCount: 7,
-        rowCount: 6,
-        columns: [
-          [], [], [], [], [],
-          [
-            { column: 5, row: 0, player: 'blue' },
-            { column: 5, row: 1, player: 'red' }
-          ],
-          []
-        ],
-        lastPlacedChip: { column: 5, row: 1, player: 'red' }
-      }
-    };
-    game.restoreFromServer({
-      game: serverGame,
-      localPlayer: serverGame.players[0]
-    });
+  it('should restore basic state from server', function () {
+    game.restoreFromServer({ game: serverGame, localPlayer });
     expect(game.inProgress).to.equal(true);
-    expect(game.players[0]).to.be.instanceOf(Player);
-    expect(game.players[1]).to.be.instanceOf(Player);
     expect(game.type).to.equal('online');
+  });
+
+  it('should restore player data from server', function () {
+    game.restoreFromServer({ game: serverGame, localPlayer });
+    expect(game.players[0]).to.be.instanceOf(HumanPlayer);
+    expect(game.players[1]).to.be.instanceOf(OnlinePlayer);
     expect(game.currentPlayer).to.equal(game.players[1]);
     expect(game.requestingPlayer).to.equal(game.players[0]);
+  });
+
+  it('should set correct player to online player', function () {
+    localPlayer = serverGame.players[1];
+    game.restoreFromServer({ game: serverGame, localPlayer });
+    expect(game.players[0]).to.be.instanceOf(OnlinePlayer);
+    expect(game.players[1]).to.be.instanceOf(HumanPlayer);
+    expect(game.currentPlayer).to.equal(game.players[1]);
+    expect(game.requestingPlayer).to.equal(game.players[0]);
+  });
+
+  it('should restore grid data from server', function () {
+    game.restoreFromServer({ game: serverGame, localPlayer });
     expect(game.grid.columns[5][0]).to.be.instanceOf(Chip);
     expect(game.grid.columns[5][0].column).to.equal(5);
     expect(game.grid.columns[5][0].row).to.equal(0);
