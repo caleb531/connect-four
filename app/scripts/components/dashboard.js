caleb531/connect-four

commit 19c8ef509fa894ebe7fa2220a8cd35d3afa850f4

Require player names to be non-empty

app/scripts/components/dashboard.js
@@ -156,6 +156,8 @@ class DashboardComponent {
           m('input[type=text][autocomplete=off]#new-player-name', {
             name: 'new-player-name',
             autofocus: true,
+            required: true,
+            pattern: '\\S+',
             oninput: (inputEvent) => this.setNewPlayerName(inputEvent)
           }),
           m('button[type=submit]', roomCode ? 'Join Game' : 'Start Game')

commit aaecfe77fe4c4c89f137ae9c3ffbec4c19e317dc

Fix bug where player name could not contain space
This fixes a regression in 19c8ef5.

app/scripts/components/dashboard.js
@@ -157,7 +157,6 @@ class DashboardComponent {
             name: 'new-player-name',
             autofocus: true,
             required: true,
-            pattern: '\\S+',
             oninput: (inputEvent) => this.setNewPlayerName(inputEvent)
           }),
           m('button[type=submit]', roomCode ? 'Join Game' : 'Start Game')