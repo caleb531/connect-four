d5736e5074122a4f7dfdc4752e04ecd4c9188cc4
Caleb Evans <caleb@calebevans.me>
2019-07-07 15:32:39 -0700

    Add test for AsyncPlayer

---
+++ test/async-player.spec.js
@@ -0,0 +1,17 @@
+import AsyncPlayer from '../app/scripts/models/async-player.js';
+
+describe('async player', function () {
+
+  class MyAsyncPlayer extends AsyncPlayer {
+    // do nothing
+  }
+
+  it('should error if getNextMove is not defined', function () {
+    const asyncPlayer = new MyAsyncPlayer({
+      name: 'Super Player',
+      color: 'blue'
+    });
+    expect(asyncPlayer.getNextMove).to.throw();
+  });
+
+});
