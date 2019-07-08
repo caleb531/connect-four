1b5c72bdf742a201c61f06ca4797d5acd8c9f0b8
Caleb Evans <caleb@calebevans.me>
2019-07-07 18:00:33 -0700

    Limit online player names to 12 characters

--- app/scripts/components/dashboard-controls.js
+++ app/scripts/components/dashboard-controls.js
@@ -121,6 +121,7 @@ class DashboardControlsComponent {
           name: 'new-player-name',
           autofocus: true,
           required: true,
+          maxlength: 12,
           oninput: (inputEvent) => this.setNewPlayerName(inputEvent)
         }),
         m('button[type=submit]', roomCode ? 'Join Game' : 'Start Game')
