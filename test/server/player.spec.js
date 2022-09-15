78b85a27fe55e25b3a2c7a6458f90760b2a1f9d9
JoelCCodes <32679235+JoelCCodes@users.noreply.github.com>
2022-09-15 18:44:02 -0300

    ...

--- test/server/player.spec.js
+++ test/server/player.spec.js
@@ -1,99 +1,100 @@
-import Player from '../../app/server/player.js';
+import Player from "../../app/server/player.js";
 
-describe('server player', function () {
-
-  it('should initialize with arguments', function () {
+describe("server player", function () {
+  it("should initialize with arguments", function () {
     const socket = sinon.stub();
     const player = new Player({
-      name: 'Caleb',
-      color: 'green',
-      socket
+      name: "8",
+      color: "green",
+      socket,
     });
-    expect(player).to.have.property('name', 'Caleb');
-    expect(player).to.have.property('color', 'green');
-    expect(player).to.have.property('socket', socket);
-    expect(player).to.have.property('score', 0);
-    expect(player).to.have.property('lastSubmittedWinner', null);
-    expect(player).to.have.property('lastDisconnectReason', null);
-    expect(player).to.have.property('lastReaction', null);
+    expect(player).to.have.property("name", "8");
+    expect(player).to.have.property("color", "green");
+    expect(player).to.have.property("socket", socket);
+    expect(player).to.have.property("score", 0);
+    expect(player).to.have.property("lastSubmittedWinner", null);
+    expect(player).to.have.property("lastDisconnectReason", null);
+    expect(player).to.have.property("lastReaction", null);
   });
 
-  it('should generate valid v4 UUID', function () {
+  it("should generate valid v4 UUID", function () {
     const player = new Player({
-      name: 'Caleb',
-      color: 'green',
-      socket: null
+      name: "8",
+      color: "green",
+      socket: null,
     });
-    expect(player.id).to.match(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
+    expect(player.id).to.match(
+      /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
+    );
   });
 
-  it('should know when socket is connected', function () {
+  it("should know when socket is connected", function () {
     const socket = sinon.stub();
     const player = new Player({
-      name: 'Caleb',
-      color: 'green',
-      socket
+      name: "8",
+      color: "green",
+      socket,
     });
-    expect(player).to.have.property('connected', true);
+    expect(player).to.have.property("connected", true);
   });
 
-  it('should know when socket is not connected', function () {
+  it("should know when socket is not connected", function () {
     const player = new Player({
-      name: 'Caleb',
-      color: 'green',
-      socket: null
+      name: "8",
+      color: "green",
+      socket: null,
     });
-    expect(player).to.have.property('connected', false);
+    expect(player).to.have.property("connected", false);
   });
 
-  it('should emit a server event for this player only', function () {
+  it("should emit a server event for this player only", function () {
     const socket = sinon.stub({
-      emit: () => {/* noop */}
+      emit: () => {
+        /* noop */
+      },
     });
     const player = new Player({
-      name: 'Caleb',
-      color: 'green',
-      socket
+      name: "8",
+      color: "green",
+      socket,
     });
-    player.emit('my-event', { foo: 'bar' });
-    expect(socket.emit).to.have.been.calledWith('my-event', { foo: 'bar' });
+    player.emit("my-event", { foo: "bar" });
+    expect(socket.emit).to.have.been.calledWith("my-event", { foo: "bar" });
   });
 
-  it('should broadcast a server event to all players', function () {
+  it("should broadcast a server event to all players", function () {
     const socket = sinon.stub({
-      emit: () => {/* noop */}
+      emit: () => {
+        /* noop */
+      },
     });
     const localPlayer = new Player({
-      name: 'Caleb',
-      color: 'green',
-      socket
+      name: "8",
+      color: "green",
+      socket,
     });
-    const players = [
-      new Player({ color: 'blue', name: 'Bob' }),
-      localPlayer
-    ];
+    const players = [new Player({ color: "blue", name: "Bob" }), localPlayer];
     localPlayer.room = { players };
-    localPlayer.broadcast('my-event', { foo: 'bar' });
-    expect(socket.emit).to.have.been.calledWith('my-event', {
-      foo: 'bar',
-      localPlayer
+    localPlayer.broadcast("my-event", { foo: "bar" });
+    expect(socket.emit).to.have.been.calledWith("my-event", {
+      foo: "bar",
+      localPlayer,
     });
   });
 
-  it('should serialize as JSON', function () {
+  it("should serialize as JSON", function () {
     const socket = sinon.stub();
     const player = new Player({
-      name: 'Caleb',
-      color: 'green',
-      socket
+      name: "8",
+      color: "green",
+      socket,
     });
     const json = player.toJSON();
-    expect(json).to.have.property('id');
-    expect(json).to.have.property('name', 'Caleb');
-    expect(json).to.have.property('color', 'green');
-    expect(json).to.have.property('connected', true);
-    expect(json).to.have.property('lastDisconnectReason', null);
-    expect(json).to.have.property('lastReaction', null);
+    expect(json).to.have.property("id");
+    expect(json).to.have.property("name", "8");
+    expect(json).to.have.property("color", "green");
+    expect(json).to.have.property("connected", true);
+    expect(json).to.have.property("lastDisconnectReason", null);
+    expect(json).to.have.property("lastReaction", null);
   });
-
 });
