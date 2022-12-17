c7bfcaeedcf1dccfcc0211daac7e686eff30fa28
Caleb Evans <caleb@calebevans.me>
2022-12-17 11:40:26 -0800

    Add missing `await` keywords to locators

--- test/ui/gameplay.spec.js
+++ test/ui/gameplay.spec.js
@@ -10,7 +10,7 @@ test.describe('game UI', async () => {
   test('should place chip in initial column', async ({ page }) => {
     await page.getByRole('button', { name: '1 Player' }).click();
     await page.getByRole('button', { name: 'Human' }).click();
-    const grid = page.locator('#grid');
+    const grid = await page.locator('#grid');
     await grid.click({ position: { x: 0, y: 0 } });
     const pendingChip = await page.locator('.chip.pending');
     await grid.click({ position: { x: 0, y: 0 } });
@@ -20,7 +20,7 @@ test.describe('game UI', async () => {
   test('should align chip to clicked column', async ({ page }) => {
     await page.getByRole('button', { name: '1 Player' }).click();
     await page.getByRole('button', { name: 'Human' }).click();
-    const grid = page.locator('#grid');
+    const grid = await page.locator('#grid');
     await grid.click({ position: { x: 192, y: 0 } });
     const pendingChip = await page.locator('.chip.pending');
     await expect(pendingChip, page).toHaveTranslate(300, 0);
@@ -29,7 +29,7 @@ test.describe('game UI', async () => {
   test('should place chip after aligning', async ({ page }) => {
     await page.getByRole('button', { name: '1 Player' }).click();
     await page.getByRole('button', { name: 'Human' }).click();
-    const grid = page.locator('#grid');
+    const grid = await page.locator('#grid');
     await grid.click({ position: { x: 192, y: 0 } });
     let pendingChip;
     pendingChip = await page.locator('.chip.pending');
@@ -43,33 +43,31 @@ test.describe('game UI', async () => {
   test('should signal AI to place chip on its turn', async ({ page }) => {
     await page.getByRole('button', { name: '1 Player' }).click();
     await page.getByRole('button', { name: 'Human' }).click();
-    const grid = page.locator('#grid');
+    const grid = await page.locator('#grid');
     let pendingChip;
     // Human's turn
+    // Human chip's initial position (before placing)
     await grid.click({ position: { x: 192, y: 0 } });
     pendingChip = await page.locator('.chip.pending');
-    // Human chip's initial position
     await expect(pendingChip).toHaveTranslate(300, 0);
+    // Place human chip
     await grid.click({ position: { x: 192, y: 0 } });
-    pendingChip = await page.locator('.chip.pending');
-    await grid.click({ position: { x: 300, y: 0 } });
-    // Human chip's final position
     await expect(pendingChip).toHaveTranslate(300, 600);
     pendingChip = await page.locator('.chip.pending');
     // AI's turn
-    // AI chip's initial position
+    // AI chip's initial position (before placing)
     await expect(pendingChip).toHaveClass(/black/);
     await expect(pendingChip).toHaveTranslate(200, 0);
     pendingChip = await page.locator('.chip.pending');
     await grid.click({ position: { x: 200, y: 0 } });
-    // AI chip's final position
+    // AI chip's final position (after placing)
     await expect(pendingChip).toHaveTranslate(200, 600);
   });
 
   test('should align chip to hovered column', async ({ page }) => {
     await page.getByRole('button', { name: '1 Player' }).click();
     await page.getByRole('button', { name: 'Human' }).click();
-    const grid = page.locator('#grid');
+    const grid = await page.locator('#grid');
     await grid.click({ position: { x: 192, y: 0 } });
     const pendingChip = await page.locator('.chip.pending');
     await expect(pendingChip).toHaveTranslate(300, 0);
