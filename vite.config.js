624c08b182bfad2ebb62f390d2df0df9818e21db
Caleb Evans <caleb@calebevans.me>
2022-12-22 21:19:04 -0800

    Build service worker using Vite PWA plugin.
    This is the first of a series of commits to upgrade the service worker
    infrastructure to something more modern.

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,42 @@
 import { defineConfig } from 'vite';
-import { injectManifest } from 'rollup-plugin-workbox';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    injectManifest({
-      globDirectory: 'dist',
-      globPatterns: [
-        '**\/*.{js,css,png}'
-      ],
-      // Precaching index.html using templatedURLs fixes a "Response served by
-      // service worker has redirections" error on iOS 12; see
-      // <https://github.com/v8/v8.dev/issues/4> and
-      // <https://github.com/v8/v8.dev/pull/7>
-      templatedURLs: {
-        // '.' must be used instead of '/' because the app is not served from
-        // the root of the domain (but rather, from a subdirectory)
-        '.': ['index.html']
+    VitePWA({
+      // Do not inject any SW registration code into the front end application,
+      // since we will already be handling this manually
+      injectRegister: null,
+      workbox: {
+        // Add additional file types to be cached by service worker (by default,
+        // the servie worker caches *.css, *.js, and *.html; see
+        // <https://vite-pwa-org.netlify.app/guide/service-worker-precache.html#precache-manifest>)
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
+        swDest: 'dist/service-worker.js'
       },
-      swSrc: 'scripts/service-worker.js',
-      swDest: 'dist/service-worker.js'
+      manifest: {
+        short_name: 'Connect Four',
+        name: 'Connect Four',
+        description: 'The slickest way to get 4-in-a-row. Play on your phone or computer, with a friend or against Mr. AI. Just be sure to enjoy and have fun.',
+        start_url: '.',
+        display: 'standalone',
+        orientation: 'portrait',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/app-icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/app-icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     })
   ]
 });
