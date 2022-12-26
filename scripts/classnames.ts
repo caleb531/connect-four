5d719b3ca1e8cc261d5ccada4fec76d4c2ed53ad
Caleb Evans <caleb@calebevans.me>
2022-12-26 11:20:40 -0800

    Convert classnames utility to TypeScript

--- scripts/classnames.ts
+++ scripts/classnames.ts
@@ -1,6 +1,6 @@
 // A minimal alternative to the classnames npm package
 // (https://www.npmjs.com/package/classnames)
-export default (classObj) => {
+export default (classObj: { [key: string]: boolean }) => {
   return (
     Object.keys(classObj)
       // Filter class names to only those with a corresponding truthy value
