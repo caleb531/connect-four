df6767705e84ef3d52f795bff942bc7f6733b302
Caleb Evans <caleb@calebevans.me>
2023-05-21 16:33:20 -0700

    Upgrade to @fontsource v5
    Also switch to the default font-display:swap, since it does not seem to
    have any adverse effect.

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 import m from 'mithril';
 import AppComponent from './components/app.js';
 import '../styles/index.scss';
+import '@fontsource/ubuntu/400.css';
 
 // Eliminate the #! for all routes
 m.route.prefix = '';
